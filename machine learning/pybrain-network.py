d31fbbd7a882b4b3e7e7cb719d8ee0a3fdedda49
YunZhang <thuzyun@gmail.com>
2016-04-26 16:14:42 +0800

    Change args

--- machine learning/pybrain-network.py
+++ machine learning/pybrain-network.py
@@ -1,152 +1,150 @@
 #Author :Zhu Zichen
 #Date : 2016/4/5
-import sys, os
+import sys, os, getopt
 import numpy as np
 import pickle
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
+
+
 def output_model(ds, model_name, input_len, output_len):
-	model_file = file(model_name, "wb")
-	neural_network = buildNetwork(input_len, 500, output_len, bias=True)
-	trainer = BackpropTrainer(neural_network, ds,weightdecay=0.1,learningrate=0.0001)
-	trainer.trainEpochs(epochs=100)
-	model_str = pickle.dumps(neural_network)
-	pickle.dump(model_str, model_file, True)
-	return neural_network
+    model_file = file(model_name, "wb")
+    neural_network = buildNetwork(input_len, 500, output_len, bias=True)
+    trainer = BackpropTrainer(neural_network, ds, weightdecay=0.1, learningrate=0.0001)
+    trainer.trainEpochs(epochs=100)
+    model_str = pickle.dumps(neural_network)
+    pickle.dump(model_str, model_file, True)
+    return neural_network
+
 
 def file_train(filename, model_name):
-	data = np.loadtxt(filename, float)
-	input_info = data[0:][..., 0:-1]
-	target = data[0:][..., [-1]]
-	input_len = len(input_info[0])
-	output_len = 1
-	data_num = len(input_info)
+    data = np.loadtxt(filename, float)
+    input_info = data[0:][..., 0:-1]
+    target = data[0:][..., [-1]]
+    input_len = len(input_info[0])
+    output_len = 1
+    data_num = len(input_info)
+    ds = SupervisedDataSet(input_len, output_len)
+    for i in range(data_num):
+        ds.addSample(input_info[i], target[i])
+    neural_network = output_model(ds, model_name, input_len, output_len)
+    return neural_network
 
-	ds = SupervisedDataSet(input_len, output_len)
-	for i in range(data_num):
-		ds.addSample(input_info[i], target[i])
 
-	neural_network = output_model(ds, model_name, input_len, output_len)
-	return neural_network
+def file_check(mode, filename, model_name="", neural_network={}):
+    data = np.loadtxt(filename, float)
+    if mode == 1:
+        pass
+    elif mode == 2 and neural_network == {} and os.path.exists(model_name):
+        model_file = file(model_name, "rb")
+        model_str = pickle.load(model_file)
+        neural_network = pickle.loads(model_str)
+    else:
+        print "Invalid parameter"
+        sys.exit()
 
-def file_check(mode, filename, model_name="",neural_network={}):
-	data = np.loadtxt(filename, float)
-	if mode == 1:
-		pass
-	elif mode == 2 and neural_network == {} and os.path.exists(model_name):
-		model_file = file(model_name, "rb")
-		model_str = pickle.load(model_file)
-		neural_network = pickle.loads(model_str)
-	else :
-		print "Invalid parameter"
-		sys.exit()
+    if os.path.exists(filename):
+        data = np.loadtxt(filename, float)
+        input_info = data[0:-1][..., 0:-1]
+        target = data[0:-1][..., [-1]]
+        input_len = len(input_info[0])
+        output_len = 1
+        data_num = len(input_info)
 
-	if os.path.exists(filename):
-		data = np.loadtxt(filename, float)
-		input_info = data[0:-1][..., 0:-1]
-		target = data[0:-1][..., [-1]]
-		input_len = len(input_info[0])
-		output_len = 1
-		data_num = len(input_info)
+        ds = SupervisedDataSet(input_len, output_len)
+        for i in range(data_num):
+            ds.addSample(input_info[i], 0)
+        print neural_network.activateOnDataset(ds)
 
-		ds = SupervisedDataSet(input_len, output_len)
-		for i in range(data_num):
-			ds.addSample(input_info[i],0)
-		print neural_network.activateOnDataset(ds)
 
 def dir_train(dir_name, model_name):
-	if os.path.exists(dir_name) :
-		flag = True
-		for parent, dirnames, filenames in os.walk(dir_name) : 
-			for filename in filenames :
-				print filename
-				data = np.loadtxt(os.path.join(dir_name, filename), float)
-				input_info = data[0:][...,0:-1]
-				target = data[0:][...,[-1]]
-				data_num = len(data)
-				if flag:
-					flag = False
-					input_len = len(input_info[0])
-					output_len = 1
-					ds = SupervisedDataSet(input_len, output_len)
-				for i in range(data_num):
-					ds.addSample(input_info[i], target[i])
-		neural_network = output_model(ds, model_name, input_len, output_len)
-		return neural_network
-	else:
-		print "Invalid directory name."
-		sys.exit()
+    if os.path.exists(dir_name) :
+        flag = True
+        for parent, dirnames, filenames in os.walk(dir_name) :
+            for filename in filenames :
+                print filename
+                data = np.loadtxt(os.path.join(dir_name, filename), float)
+                input_info = data[0:][...,0:-1]
+                target = data[0:][...,[-1]]
+                data_num = len(data)
+                if flag:
+                    flag = False
+                    input_len = len(input_info[0])
+                    output_len = 1
+                    ds = SupervisedDataSet(input_len, output_len)
+                for i in range(data_num):
+                    ds.addSample(input_info[i], target[i])
+        neural_network = output_model(ds, model_name, input_len, output_len)
+        return neural_network
+    else:
+        print "Invalid directory name."
+        sys.exit()
+
 
 def dir_check(mode, dir_name, model_name="", neural_network={}):
-	if mode == 1:
-		pass
-	elif mode == 2 and neural_network == {} and os.path.exists(model_name):
-		model_file = file(model_name, "rb")
-		model_str = pickle.load(model_file)
-		neural_network = pickle.loads(model_str)
-	else :
-		print "Invalid parameter"
-		sys.exit()
+    if mode == 1:
+        pass
+    elif mode == 2 and neural_network == {} and os.path.exists(model_name):
+        model_file = file(model_name, "rb")
+        model_str = pickle.load(model_file)
+        neural_network = pickle.loads(model_str)
+    else :
+        print "Invalid parameter"
+        sys.exit()
 
-	if os.path.exists(dir_name) :
-		for parent, dirnames, filenames in os.walk(dir_name) : 
-			for filename in filenames :
-				data = np.loadtxt(os.path.join(dir_name, filename), float)
-				input_info = data[0:-1][..., 0:-1]
-				target = data[0:-1][..., [-1]]
-				input_len = len(input_info[0])
-				output_len = 1
-				data_num = len(input_info)
+    if os.path.exists(dir_name) :
+        for parent, dirnames, filenames in os.walk(dir_name) :
+            for filename in filenames :
+                data = np.loadtxt(os.path.join(dir_name, filename), float)
+                input_info = data[0:-1][..., 0:-1]
+                target = data[0:-1][..., [-1]]
+                input_len = len(input_info[0])
+                output_len = 1
+                data_num = len(input_info)
 
-				ds = SupervisedDataSet(input_len, output_len)
-				for i in range(data_num):
-					ds.addSample(input_info[i],0)
-				print neural_network.activateOnDataset(ds)
-	else:
-		print "Invalid directory name."
-		sys.exit()
+                ds = SupervisedDataSet(input_len, output_len)
+                for i in range(data_num):
+                    ds.addSample(input_info[i],0)
+                print neural_network.activateOnDataset(ds)
+    else:
+        print "Invalid directory name."
+        sys.exit()
 
-def main(mode, trained_data="", check_data="", model_name=""):
-	if mode == "1":
-		neural_network = file_train(trained_data, model_name)
-		file_check(1, check_data, model_name, neural_network)
-	elif mode == "2":
-		neural_network = dir_train(trained_data, model_name)
-		dir_check(1, check_data, model_name, neural_network)
-	elif mode == "3":
-		file_train(trained_data, model_name)
-	elif mode == "4":
-		dir_train(trained_data, model_name)
-	elif mode == "5":
-		file_check(2, check_data, model_name)
-	elif mode == "6":
-		dir_check(2, check_data, model_name)
-	else :
-		print "Invalid directory name."
-		sys.exit()
 
 if __name__ == '__main__':
-	length = len(sys.argv)
-	trained_data = ""
-	check_data = ""
-	model_name = "model.pkl"
-	if length == 3:
-		if sys.argv[1] == "3" or sys.argv[1] == "4" :
-			trained_data = sys.argv[2]
-		elif sys.argv[1] == "5" or sys.argv[1] == "6" :
-			check_data = sys.argv[2]
-		else :
-			print "Invalid directory name."
-			sys.exit()
-	elif length == 4:
-		if sys.argv[1] != "1" and sys.argv[1] != "2":
-			print "Invalid directory name."
-			sys.exit()
-
-		trained_data = sys.argv[2]
-		check_data = sys.argv[3]
-	else :
-		print "Invalid directory name."
-		sys.exit()
-	main(sys.argv[1], trained_data, check_data, model_name)
+    opts, args = getopt.getopt(sys.argv[1:], "rct")
+    directory_flag = False
+    check_flag = False
+    train_flag = False
+    model_name = "model.pkl"
+    for opt, value in opts:
+        if opt == "-r":
+            directory_flag = True
+        elif opt == "-c":
+            check_flag = True
+        elif opt == "-t":
+            train_flag = True
+        else:
+            pass
+    if train_flag and len(args) >= 1:
+        trained_data = args[0]
+        if directory_flag:
+            neural_network = dir_train(trained_data, model_name)
+            if check_flag and len(args) == 2:
+                check_data = args[1]
+                dir_check(1, check_data, model_name, neural_network)
+        else:
+            neural_network = file_train(trained_data, model_name)
+            if check_flag and len(args) == 2:
+                check_data = args[1]
+                file_check(1, check_data, model_name, neural_network)
+    elif check_flag and len(args) == 1:
+        check_data = args[0]
+        if directory_flag:
+            dir_check(2, check_data, model_name)
+        else:
+            file_check(2, check_data, model_name)
+    else:
+        print("Invalid parameters")
+        sys.exit()
--- machine learning/supplement.py
+++ machine learning/supplement.py
@@ -1,81 +1,73 @@
-#Author :Zhu Zichen
-#Date : 2016/3/23
-#mode 1 : file 
-#mode 2: directory 
-import os, sys
-
-def getData(input_filename):
-	if os.path.exists(input_filename) :
-		inputFile = open(input_filename, "r")
-		origin_data = inputFile.readlines() 
-		length = len(origin_data)
-		result = list()
-		i = 1
-		for line in origin_data:
-			percent = "%.2f" % ( (i*1.0/length)*100 )
-			result.append(line.strip() + " " + str(percent))
-			i += 1
-		inputFile.close()
-	else:
-		print "Invalid File name."
-		sys.exit()
-	return '\n'.join(result)
-
-def fileSupplement(input_filename, output_filename):
-	data = getData(input_filename)
-	outputFile = open((output_filename), "w")
-	outputFile.write(data)
-	outputFile.close()
-
-def DirectorySupplement(input_directory, output_directory):
-	try:
-		os.makedirs(output_directory)
-	except Exception as e:
-		print e
-		sys.exit()
-
-	if os.path.exists(input_directory) :
-		for parent, dirnames, filenames in os.walk(input_directory) : 
-			print parent
-			for filename in filenames :
-				print filename
-				output_filename = filename[:-4] + " OUT.txt"
-				print output_filename
-				data = getData(os.path.join(parent,filename))
-				output_filepath = os.path.join(output_directory,output_filename)
-				outputFile = open(output_filepath, "w")
-				outputFile.write(data)
-				outputFile.close()
-
-	else:
-		print "Invalid File name."
-
-def main(mode, input_name, output_name):
-	if mode == "1" :
-		if input_name == output_name:
-			output_name = input_name[:-4] + " OUT.txt"
-		fileSupplement(input_name, output_name)
-	elif mode == "2" :
-		if input_name == output_name:
-			output_name = input_name + " OUT"
-		DirectorySupplement(input_name, output_name)
-	else :
-		print "Invalid mode."
+# Author :Zhu Zichen
+# Date : 2016/3/23
+# mode 1 : file
+# mode 2: directory
+import os, sys, getopt
 
 
+def get_data(input_filename):
+    if os.path.exists(input_filename):
+        input_file = open(input_filename, "r")
+        origin_data = input_file.readlines()
+        length = len(origin_data)
+        result = list()
+        i = 1
+        for line in origin_data:
+            percent = "%.2f" % ((i * 1.0 / length) * 100)
+            result.append(line.strip() + " " + str(percent))
+            i += 1
+        input_file.close()
+    else:
+        print "Invalid File name."
+        sys.exit()
+    return '\n'.join(result)
+
+
+def file_supplement(input_filename, output_filename):
+    data = get_data(input_filename)
+    output_file = open((output_filename), "w")
+    output_file.write(data)
+    output_file.close()
+
+
+def directory_supplement(input_directory, output_directory):
+    try:
+        os.makedirs(output_directory)
+    except Exception as e:
+        print e
+        sys.exit()
+
+    if os.path.exists(input_directory):
+        for parent, dirnames, filenames in os.walk(input_directory):
+            for filename in filenames:
+                output_filename = filename[:-4] + "-out"
+                data = get_data(os.path.join(parent, filename))
+                output_filepath = os.path.join(output_directory, output_filename)
+                output_file = open(output_filepath, "w")
+                output_file.write(data)
+                output_file.close()
+
+    else:
+        print "Invalid File name."
 
 
 if __name__ == '__main__':
- 	length = len(sys.argv)
-	if length <= 2:
-		print "Invalid parameters."
-		sys.exit()
-	elif length == 3:
-		input_name = sys.argv[2]
-		output_name = input_name
-	elif length == 4:
-		print sys.argv[1]
-		input_name = sys.argv[2]
-		output_name = sys.argv[3]
-	main(sys.argv[1], input_name, output_name)
-	
+    opts, args = getopt.getopt(sys.argv[1:], "r")
+    if len(args) == 1:
+        input_name = args[0]
+        output_name = input_name + "-out"
+    elif len(args) == 2 and args[0] != args[1]:
+        input_name = args[0]
+        output_name = args[1]
+    else:
+        print "Invalid parameters."
+        sys.exit()
+
+    directory_flag = False
+    for opt, value in opts:
+        if opt == "-r":
+            directory_flag = True
+    if directory_flag:
+        directory_supplement(input_name, output_name)
+    else:
+        file_supplement(input_name, output_name)
